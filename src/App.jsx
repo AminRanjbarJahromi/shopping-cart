7545a2fb70c2fc4e391675d83b7e6eac51cf5c0d
Amin Ranjbar <amin.ranjbar.dev@gmail.com>
2024-01-04 20:59:39 +0100

    improve component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import{Routes, Route} from 'react-router-dom'
 import {Container} from 'react-bootstrap'
 
 import Navbar from './components/Navbar'
+import Shop from './pages/shop';
 
 
 
@@ -9,6 +11,9 @@ function App() {
   return (
     <Container>
       <Navbar></Navbar>
+      <Routes>
+        <Route index element ={<Shop />} />
+      </Routes>
     </Container>
   );
 }
